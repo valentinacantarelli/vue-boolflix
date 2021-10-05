70a2d763aad1946a62faef9ee3d77b5128d54ace
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-05 15:03:28 +0200

    creata struttura

---
+++ src/components/Films.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+    name:"Films",
+}
+</script>
+
+<style lang="scss" scope>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <input type="text" placeholder="cosa vuoi guardare oggi?" v-model="inputText">
+      <button @click="$emit('search', inputText)">Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Header",
+    data(){
+      return{
+        inputText:""
+      } 
+    },
+}
+</script>
+
+<style lang="scss" scope>
+
+</style>
